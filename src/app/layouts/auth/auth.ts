71c5ad973d2ec6ed56da255f83c1df9e39d43b58
Sophia Portela <sophianobreportela@gmail.com>
2025-09-14 22:42:08 -0300

    feat: add auth layout

---
+++ src/app/layouts/auth/auth.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './auth.html',
+  styleUrl: './auth.sass'
+})
+export class Auth {
+
+}
