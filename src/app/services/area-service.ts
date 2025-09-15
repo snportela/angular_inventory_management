88fd4e8053a3c9bf581afe9766df36d58adf6c0b
Sophia Portela <sophianobreportela@gmail.com>
2025-09-14 22:42:47 -0300

    feat: create area-service

---
+++ src/app/services/area-service.ts
@@ -0,0 +1,25 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../environments/environment.development';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AreaList} from '../../models/area-list';
+import {toSignal} from '@angular/core/rxjs-interop';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AreaService {
+
+  private api_url: string = environment.API_URL;
+  http: HttpClient = inject(HttpClient);
+
+  private areaList$: Observable<AreaList> = this.getAreaList();
+  public areaList = toSignal(this.areaList$, {
+    initialValue: {totalItems: 0, totalPages: 0, areas: [], currentPage: 0},
+  });
+
+  getAreaList(): Observable<AreaList> {
+    return this.http.get<AreaList>(this.api_url + '/areas');
+  }
+
+}
