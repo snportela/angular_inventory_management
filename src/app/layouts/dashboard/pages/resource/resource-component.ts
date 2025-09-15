fbea5dc01059d6b984bf5c2fa9df6d4bd7ff69a3
Sophia Portela <sophianobreportela@gmail.com>
2025-09-14 22:49:06 -0300

    feat: create resource component

---
+++ src/app/layouts/dashboard/pages/resource/resource-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource',
+  imports: [],
+  templateUrl: './resource-component.html',
+  styleUrl: './resource-component.sass'
+})
+export class ResourceComponent {
+
+}
