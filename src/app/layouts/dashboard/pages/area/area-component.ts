4c473a924381379e7871e1a74915681a8be08625
Sophia Portela <sophianobreportela@gmail.com>
2025-09-14 22:48:11 -0300

    feat: create area component, create areas table

---
+++ src/app/layouts/dashboard/pages/area/area-component.ts
@@ -0,0 +1,28 @@
+import {Component, computed, inject, Signal} from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {AreaService} from '../../../../services/area-service';
+import {AreaList} from '../../../../../models/area-list';
+import {Area} from '../../../../../models/area-response';
+import {NgStyle} from '@angular/common';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-area',
+  imports: [
+    TableModule,
+    NgStyle,
+    IconField,
+    InputIcon,
+    InputText
+  ],
+  templateUrl: './area-component.html',
+  styleUrl: './area-component.sass'
+})
+export class AreaComponent {
+
+  areaService: AreaService = inject(AreaService);
+  areaList: Signal<AreaList> = computed(() => this.areaService.areaList());
+
+}
