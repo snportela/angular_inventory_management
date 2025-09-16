fad9b7246c078478dfd7e8cec2b8b806afa44a5c
Sophia Portela <sophianobreportela@gmail.com>
2025-09-16 20:11:12 -0300

    feat: create category table

--- src/app/layouts/dashboard/pages/category/category-component.ts
+++ src/app/layouts/dashboard/pages/category/category-component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, Signal} from '@angular/core';
+import {CategoryService} from '../../../../services/category-service';
+import {CategoryList} from '../../../../models/category-list';
+import {TableModule} from 'primeng/table';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [
+    TableModule,
+    IconField,
+    InputIcon,
+    InputText,
+    NgStyle
+  ],
   templateUrl: './category-component.html',
   styleUrl: './category-component.sass'
 })
 export class CategoryComponent {
+
+  categoryService: CategoryService = inject(CategoryService);
+  categoryList: Signal<CategoryList> = computed(() => this.categoryService.categoryList());
 
 }
