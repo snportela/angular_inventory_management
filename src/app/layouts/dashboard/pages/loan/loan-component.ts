e6415259645798f618aa0f82178bf9a3d31e061a
Sophia Portela <sophianobreportela@gmail.com>
2025-09-16 21:03:17 -0300

    feat: create loan table

--- src/app/layouts/dashboard/pages/loan/loan-component.ts
+++ src/app/layouts/dashboard/pages/loan/loan-component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, Signal} from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {LoanService} from '../../../../services/loan-service';
+import {LoanList} from '../../../../models/loan/loan-list';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
+import {DatePipe, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-loan',
-  imports: [],
+  imports: [
+    TableModule,
+    IconField,
+    InputIcon,
+    InputText,
+    NgStyle,
+    DatePipe
+  ],
   templateUrl: './loan-component.html',
   styleUrl: './loan-component.sass'
 })
 export class LoanComponent {
+
+  loanService: LoanService = inject(LoanService);
+  loanList: Signal<LoanList> = computed(() => this.loanService.loanList());
 
 }
