6a770d533a520a4d677b0c47a47b0c6d0dac480b
Sophia Portela <sophianobreportela@gmail.com>
2025-09-26 19:24:34 -0300

    feat: create login form

--- src/app/layouts/auth/pages/login-component/login-component.ts
+++ src/app/layouts/auth/pages/login-component/login-component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoginService} from '../../../../services/login-service';
+import {Login} from '../../../../models/auth/login';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    ReactiveFormsModule
   ],
   templateUrl: './login-component.html',
   styleUrl: './login-component.sass'
 })
 export class LoginComponent {
+
+  loginService: LoginService = inject(LoginService);
+  router = inject(Router);
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl("", [Validators.required]),
+    password: new FormControl("", Validators.required)
+  })
+
+  onSubmit() {
+
+    const credentials = this.loginForm.value as Login;
+
+    this.loginService.login(credentials).subscribe({
+      next: (res: any) => {
+        if(res.token) {
+          localStorage.setItem('token', res.token);
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          console.log("Login failed")
+        }
+      },
+      error:(error) => {
+        console.log(error);
+      }
+    })
+  }
 
 }
