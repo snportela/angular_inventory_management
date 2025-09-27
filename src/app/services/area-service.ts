361ab0f3bb482acb2ce22757a55843cdeb176025
Sophia Portela <sophianobreportela@gmail.com>
2025-09-26 22:30:05 -0300

    feat: add remaining methods to area service

--- src/app/services/area-service.ts
+++ src/app/services/area-service.ts
@@ -3,6 +3,7 @@ import {environment} from '../environments/environment.development';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AreaList} from '../models/area/area-list';
+import {Area} from '../models/area/area-response';
 import {toSignal} from '@angular/core/rxjs-interop';
 
 @Injectable({
@@ -13,13 +14,24 @@ export class AreaService {
   private api_url: string = environment.API_URL;
   http: HttpClient = inject(HttpClient);
 
-  private areaList$: Observable<AreaList> = this.getAreaList();
-  public areaList = toSignal(this.areaList$, {
-    initialValue: {totalItems: 0, totalPages: 0, areas: [], currentPage: 0},
-  });
-
   getAreaList(): Observable<AreaList> {
     return this.http.get<AreaList>(this.api_url + '/areas');
+  }
+
+  getArea(id: string): Observable<Area> {
+    return this.http.get<Area>(`${this.api_url}/areas/${id}`);
+  }
+
+  createArea(newArea: Omit<Area, 'areaId'>): Observable<Area> {
+    return this.http.post<Area>(this.api_url + '/areas', newArea);
+  }
+
+  updateArea(id: string, updatedArea: Omit<Area, 'areaId'>): Observable<Area>{
+    return this.http.put<Area>(`${this.api_url}/areas/${id}`, updatedArea);
+  }
+
+  deleteArea(id: string): Observable<Area> {
+    return this.http.delete<Area>(`${this.api_url}/areas/${id}`);
   }
 
 }
