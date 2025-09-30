c93ff778b7d85e133e37ea0ed35fcbc0ee718f13
Sophia Portela <sophianobreportela@gmail.com>
2025-09-30 18:16:30 -0300

    feat: implement methods to category service

--- src/app/services/category-service.ts
+++ src/app/services/category-service.ts
@@ -3,7 +3,7 @@ import {CategoryList} from '../models/category/category-list';
 import {environment} from '../environments/environment.development';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {toSignal} from '@angular/core/rxjs-interop';
+import {Category} from '../models/category/category';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +13,24 @@ export class CategoryService {
   private api_url: string = environment.API_URL;
   http: HttpClient = inject(HttpClient);
 
-  private categoryList$: Observable<CategoryList> = this.getCategoryList();
-  public categoryList = toSignal(this.categoryList$, {
-    initialValue: {totalItems: 0, totalPages: 0, categories: [], currentPage: 0},
-  })
+  getCategoryList(page: number, size: number): Observable<CategoryList> {
+      return this.http.get<CategoryList>(`${this.api_url}/categories?page=${page}&size=${size}`);
+  }
 
-  getCategoryList(): Observable<CategoryList> {
-      return this.http.get<CategoryList>(this.api_url + '/categories');
+  getCategory(id: string): Observable<Category> {
+    return this.http.get<Category>(`${this.api_url}/categories/${id}`);
+  }
+
+  createCategory(newCategory: Omit<Category, 'categoryId'>): Observable<Category> {
+    return this.http.post<Category>(this.api_url + '/categories', newCategory);
+  }
+
+  updateCategory(id: string, updatedCategory: Omit<Category, 'categoryId'>): Observable<Category> {
+    return this.http.put<Category>(`${this.api_url}/categories/${id}`, updatedCategory);
+  }
+
+  deleteCategory(id: string): Observable<Category> {
+    return this.http.delete<Category>(`${this.api_url}/categories/${id}`);
   }
 
 }
