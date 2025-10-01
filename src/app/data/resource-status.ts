f8d559fcb22c84d9e37be72623de49099e86be82
Sophia Portela <sophianobreportela@gmail.com>
2025-09-30 22:10:03 -0300

    feat: create status data

---
+++ src/app/data/resource-status.ts
@@ -0,0 +1,33 @@
+export const resourceStatus = [
+  {
+    name: 'DISCARDED',
+    translation: 'Descartado'
+  },
+  {
+    name: 'UNDER_MAINTENANCE',
+    translation: 'Em manutenção'
+  },
+  {
+    name: 'BEING_USED',
+    translation:  'Em uso'
+  },
+  {
+    name: 'NEW_UNUSED',
+    translation: 'Novo - Não utilizado'
+  },
+  {
+    name:  'NEW_NOT_INSTALLED',
+    translation: 'Novo - Não Instalado'
+  },
+  {
+    name: 'NOT_USED_GOOD_CONDITION',
+    translation: 'Parado em bom estado'
+  },
+  {
+    name: 'NOT_USED_UNFIT_FOR_USE',
+    translation: 'Parado e sem condição de uso'
+  },
+  {
+    name: 'NOT_USED_NO_UTILIZATION',
+    translation: 'Parado sem utilização'
+  }]
