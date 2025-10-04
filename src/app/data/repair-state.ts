bed8e032ee41f2f6ae090dfd617e378c8a3661da
Sophia Portela <sophianobreportela@gmail.com>
2025-10-03 23:07:17 -0300

    feat: create repair state data

---
+++ src/app/data/repair-state.ts
@@ -0,0 +1,14 @@
+export const repairState = [
+  {
+    name: 'OPERATIONAL',
+    translation: 'Operacional'
+  },
+  {
+    name: 'NEEDS_MAINTENANCE',
+    translation: 'Necessita manutenção'
+  },
+  {
+    name: 'BEYOND_REPAIR',
+    translation: 'Não tem conserto'
+  }
+]
