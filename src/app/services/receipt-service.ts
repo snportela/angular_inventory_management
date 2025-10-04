9ec329ab953dbb0ea56019f0d365a0385973c202
Sophia Portela <sophianobreportela@gmail.com>
2025-10-03 23:08:28 -0300

    feat: add methods to service

--- src/app/services/receipt-service.ts
+++ src/app/services/receipt-service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment.development';
 import {Observable} from 'rxjs';
 import {ReceiptList} from '../models/receipt/receipt-list';
+import {Receipt} from '../models/receipt/receipt';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,24 @@ export class ReceiptService {
   private api_url: string = environment.API_URL;
   private http: HttpClient = inject(HttpClient);
 
-  getReceiptList():Observable<ReceiptList> {
-    return this.http.get<ReceiptList>(this.api_url + '/receipts');
+  getReceiptList(page: number, size: number):Observable<ReceiptList> {
+    return this.http.get<ReceiptList>(`${this.api_url}/receipts?page=${page}&size=${size}`);
+  }
+
+  getReceipt(id: string): Observable<Receipt> {
+    return this.http.get<Receipt>(`${this.api_url}/receipts/${id}`);
+  }
+
+  createReceipt(newReceipt: Omit<Receipt, 'receiptId'>): Observable<Receipt> {
+    return this.http.post<Receipt>(this.api_url + '/receipts', newReceipt);
+  }
+
+  updateReceipt(id: string, updatedReceipt: Omit<Receipt, 'receiptId'>): Observable<Receipt> {
+    return this.http.put<Receipt>(`${this.api_url}/receipts/${id}`, updatedReceipt);
+  }
+
+  deleteReceipt(id: string): Observable<Receipt> {
+    return this.http.delete<Receipt>(`${this.api_url}/receipts/${id}`);
   }
 
 }
