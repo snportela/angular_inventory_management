ac788e229237c3443ae1b620219492087bd89033
Sophia Portela <sophianobreportela@gmail.com>
2025-10-07 12:22:41 -0300

    feat: implement delete resource confirmation modal

--- src/app/layouts/dashboard/pages/resource/resource-component.ts
+++ src/app/layouts/dashboard/pages/resource/resource-component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Signal} from '@angular/core';
+import {Component, effect, inject, signal, Signal, WritableSignal} from '@angular/core';
 import {ResourceService} from '../../../../services/resource-service';
 import {ResourceList} from '../../../../models/resource/resource-list';
 import {TableModule} from 'primeng/table';
@@ -18,6 +18,7 @@ import {resourceStatus} from '../../../../data/resource-status';
 import {useTime} from '../../../../data/use-time';
 import {UseTimePipe} from '../../../../pipes/use-time-pipe';
 import {AreaList} from '../../../../models/area/area-list';
+import {ConfirmationService, MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-resource',
@@ -41,18 +42,66 @@ export class ResourceComponent {
   resourceService: ResourceService = inject(ResourceService);
   areaService: AreaService = inject(AreaService);
   categoryService: CategoryService = inject(CategoryService);
+  private confirmationService = inject(ConfirmationService);
+  private messageService = inject(MessageService);
 
-  page: number = 1;
-  size: number = 10;
+  first: number = 0;
+  page = signal(0);
+  size = signal(10);
 
-  resourceList: Signal<ResourceList> = toSignal(this.resourceService.getResourceList(this.page, this.size), {initialValue: {} as ResourceList});
+  resourceList: WritableSignal<ResourceList> =  signal({currentPage: 0, totalItems: 0, totalPages: 0, resources: [] });
 
-  areas: Signal<AreaList> = toSignal(this.areaService.getAreaList(this.page, this.size), {initialValue: {} as AreaList});
+  areas: Signal<AreaList> = toSignal(this.areaService.getAreaList(this.page(), this.size()), {initialValue: {} as AreaList});
 
-  categories: Signal<CategoryList> = toSignal(this.categoryService.getCategoryList(this.page, this.size),
+  categories: Signal<CategoryList> = toSignal(this.categoryService.getCategoryList(this.page(), this.size()),
     {initialValue: {} as CategoryList});
 
   status = resourceStatus;
   useTime = useTime;
+
+  constructor() {
+    effect(() => {
+      this.resourceService.getResourceList(this.page(), this.size()).subscribe(data => this.resourceList.set(data));
+    });
+  }
+
+  confirmDelete(resource: {resourceId: string; name: string }) {
+    this.confirmationService.confirm({
+      message: `Tem certeza que deseja excluir o recurso "${resource.name}"?`,
+      header: 'Confirmar Exclusão',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sim, excluir',
+      rejectLabel: 'Cancelar',
+      acceptButtonStyleClass: 'p-button-danger',
+      rejectButtonStyleClass: 'p-button-secondary p-button-outlined',
+
+      accept: () => {
+        this.resourceService.deleteResource(resource.resourceId).subscribe({
+          next: () => {
+            this.resourceList.update(currentState => {
+              const updatedResources = currentState.resources.filter(r => r.resourceId !== resource.resourceId);
+              return {...currentState, resources: updatedResources};
+            });
+
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: `Recurso "${resource.name}" excluído.`,
+              life: 3000
+            });
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Não foi possível excluir o recurso.',
+              life: 3000
+            });
+            console.error('Delete failed', err);
+          }
+        });
+      }
+    });
+  }
 
 }
