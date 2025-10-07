6410b1f63187762ac0561d6aa2a4540006ed8440
Sophia Portela <sophianobreportela@gmail.com>
2025-10-07 16:27:35 -0300

    feat: implement feedback message on login page

--- src/app/layouts/auth/auth.ts
+++ src/app/layouts/auth/auth.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Toast} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-auth',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    Toast
+  ],
+  providers: [
+    MessageService
   ],
   templateUrl: './auth.html',
   styleUrl: './auth.sass'
--- src/app/layouts/auth/pages/login-component/login-component.ts
+++ src/app/layouts/auth/pages/login-component/login-component.ts
@@ -1,15 +1,20 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../../../services/auth-service';
 import {Login} from '../../../../models/auth/login';
 import {Router} from '@angular/router';
+import {MessageService} from 'primeng/api';
+import {ProgressSpinner} from 'primeng/progressspinner';
+import {Message} from 'primeng/message';
 
 @Component({
   selector: 'app-login-component',
   imports: [
     NgOptimizedImage,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ProgressSpinner,
+    Message
   ],
   templateUrl: './login-component.html',
   styleUrl: './login-component.sass'
@@ -17,6 +22,7 @@ import {Router} from '@angular/router';
 export class LoginComponent {
 
   authService: AuthService = inject(AuthService);
+  private messageService = inject(MessageService);
   router = inject(Router);
 
   loginForm: FormGroup = new FormGroup({
@@ -24,7 +30,13 @@ export class LoginComponent {
     password: new FormControl("", Validators.required)
   })
 
+  isLoading: WritableSignal<boolean> = signal(false);
+  isInvalid: WritableSignal<boolean> = signal(false);
+
   onSubmit() {
+
+    this.isLoading.set(true);
+    this.isInvalid.set(false);
 
     const credentials = this.loginForm.value as Login;
 
@@ -33,12 +45,22 @@ export class LoginComponent {
         if(res.token) {
           this.authService.saveToken(res.token);
           this.router.navigateByUrl('/dashboard');
+          this.isLoading.set(false);
+
         } else {
-          console.log("Login failed")
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Não foi possível fazer o login.',
+            life: 3000
+          });
+
+          this.isLoading.set(false);
         }
       },
       error:(error) => {
-        console.log(error);
+       this.isInvalid.set(true);
+        this.isLoading.set(false);
       }
     })
   }
