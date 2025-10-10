74a8a9209d2a4a8faf0a21d379c7ed2626a08f6d
Sophia Portela <sophianobreportela@gmail.com>
2025-10-10 12:10:48 -0300

    feat: add methods to loan service

--- src/app/services/loan-service.ts
+++ src/app/services/loan-service.ts
@@ -3,7 +3,7 @@ import {environment} from '../environments/environment.development';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {LoanList} from '../models/loan/loan-list';
-import {toSignal} from '@angular/core/rxjs-interop';
+import {Loan} from '../models/loan/loan';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +13,29 @@ export class LoanService {
   private api_url: string = environment.API_URL;
   http: HttpClient = inject(HttpClient);
 
-  private loanList$: Observable<LoanList> = this.getLoanList();
-  public loanList = toSignal(this.loanList$, {
-    initialValue: {totalItems: 0, totalPages: 0, loans: [], currentPage: 0},
-  });
-
-  getLoanList(): Observable<LoanList>
+  getLoanList(page: number, size: number, loanStatus?: string): Observable<LoanList>
   {
-    return this.http.get<LoanList>(this.api_url + '/loans');
+    if(loanStatus) {
+      return this.http.get<LoanList>(`${this.api_url}/loans?page=${page}&size=${size}&loanStatus=${loanStatus}`);
+    }
+
+    return this.http.get<LoanList>(`${this.api_url}/loans?page=${page}&size=${size}`);
+  }
+
+  getLoan(id: string): Observable<Loan> {
+    return this.http.get<Loan>(`${this.api_url}/loans/${id}`);
+  }
+
+  createLoan(newLoan: Omit<Loan, 'loanId'>): Observable<Loan> {
+    return this.http.post<Loan>(this.api_url + '/loans', newLoan);
+  }
+
+  updateLoan(id: string, updatedLoan: Omit<Loan, 'loanId'>): Observable<Loan> {
+    return this.http.put<Loan>(`${this.api_url}/loans/${id}`, updatedLoan);
+  }
+
+  deleteLoan(id: string): Observable<Loan> {
+    return this.http.delete<Loan>(`${this.api_url}/loans/${id}`);
   }
 
 }
