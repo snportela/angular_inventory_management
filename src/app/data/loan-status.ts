ee915dc3a307ef2c61bff8e668fb61107bc80265
Sophia Portela <sophianobreportela@gmail.com>
2025-10-10 13:04:56 -0300

    feat: create loan status data

---
+++ src/app/data/loan-status.ts
@@ -0,0 +1,22 @@
+export const loanStatus = [
+  {
+    name: 'CHECKED_OUT',
+    translation: 'Retirado',
+    severity: 'info'
+  },
+  {
+    name: 'OVERDUE',
+    translation: 'Atrasado',
+    severity: 'warn'
+  },
+  {
+    name: 'RETURNED',
+    translation: 'Devolvido',
+    severity: 'success'
+  },
+  {
+    name: 'LOST',
+    translation: 'Perdido',
+    severity: 'danger'
+  }
+]
