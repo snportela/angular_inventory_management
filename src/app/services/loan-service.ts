0a671049c07c3888572d784276094ba77648edd4
Sophia Portela <sophianobreportela@gmail.com>
2025-10-15 20:36:17 -0300

    feat: create loan item methods

--- src/app/services/loan-service.ts
+++ src/app/services/loan-service.ts
@@ -4,6 +4,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {LoanList} from '../models/loan/loan-list';
 import {Loan} from '../models/loan/loan';
+import {LoanItem} from '../models/loan/loan-item';
+import {LoanItemResponse} from '../models/loan/loan-item-response';
 
 @Injectable({
   providedIn: 'root'
@@ -36,6 +38,18 @@ export class LoanService {
 
   deleteLoan(id: string): Observable<Loan> {
     return this.http.delete<Loan>(`${this.api_url}/loans/${id}`);
+  }
+
+  addLoanItems(loanItem: LoanItem): Observable<LoanItem> {
+    return this.http.post<LoanItem>(this.api_url + '/loan-items', loanItem);
+  }
+
+  getItemsByLoanId(id: string): Observable<LoanItemResponse[]> {
+    return this.http.get<LoanItemResponse[]>(`${this.api_url}/loan-items/loan/${id}`)
+  }
+
+  removeAllItemsFromLoan(id: string) {
+    return this.http.delete(`${this.api_url}/loan-items/loan/${id}`);
   }
 
 }
