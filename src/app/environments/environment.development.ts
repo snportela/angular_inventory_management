66034008e5d4d483b5994e5904fadc175d064ba6
Sophia Portela <sophianobreportela@gmail.com>
2025-11-25 21:08:41 -0300

    chore: update variables

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  API_URL: 'http://localhost:8080/api',
-  AUTH_URL: 'http://localhost:8080/auth'
+  // API_URL: 'http://localhost:8080/api',
+  // AUTH_URL: 'http://localhost:8080/auth'
+  API_URL: 'https://spring-boot-inventory-management.onrender.com/api',
+  AUTH_URL: 'https://spring-boot-inventory-management.onrender.com/auth'
 }
